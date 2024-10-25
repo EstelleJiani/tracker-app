9d5fa736b82c6721eeb5037c344f799d78b622b3
EstelleJiani <jianiguoestelle@gmail.com>
2024-10-25 16:07:23 -0700

    Change ActivitiesScreen data fetching from React Context to Firestore.

--- screens/ActivitiesScreen.js
+++ screens/ActivitiesScreen.js
@@ -1,13 +1,23 @@
+import { useEffect, useState } from 'react';
 import { View } from 'react-native';
-import { useData } from '../components/DataContext';
+import { listenToCollection } from '../firebase/firebaseHelper';
 import { useTheme } from '../components/ThemeContext';
 import { globalStyles } from '../styles/globalStyles';
 import ItemsList from '../components/ItemsList';
 
+// The ActivitiesScreen
 function ActivitiesScreen() {
-  const { activities } = useData();
+  const [activities, setActivities] = useState([]);
   const { theme } = useTheme();
   const styles = globalStyles(theme);
+
+  useEffect(() => {
+    const unsubscribe = listenToCollection('activities', (docs) => {
+      setActivities(docs);
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   return (
     <View style={styles.container}>
