cc6d8545d4775df6d006932f7ab14a6565b92593
EstelleJiani <jianiguoestelle@gmail.com>
2024-10-25 16:10:10 -0700

    Change DietScreen data fetching from React Context to Firestore.

--- screens/DietScreen.js
+++ screens/DietScreen.js
@@ -1,13 +1,23 @@
+import { useEffect, useState } from 'react';
 import { View } from 'react-native';
-import { useData } from '../components/DataContext';
+import { listenToCollection } from '../firebase/firebaseHelper';
 import { useTheme } from '../components/ThemeContext';
 import { globalStyles } from '../styles/globalStyles';
 import ItemsList from '../components/ItemsList';
 
+// The DietScreen
 function DietScreen() {
-  const { diets } = useData();
+  const [diets, setDiets] = useState([]);
   const { theme } = useTheme();
   const styles = globalStyles(theme);
+
+  useEffect(() => {
+    const unsubscribe = listenToCollection('diets', (docs) => {
+      setDiets(docs);
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   return (
     <View style={styles.container}>
